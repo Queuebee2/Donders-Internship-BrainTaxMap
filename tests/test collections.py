53bd5771c6fb520b899c89b67b994beb23af1a61
Milain Lambers <milain.l.ambers@gmail.com>
2021-03-30 16:44:43 +0200

    Remove old stuff

--- tests/test collections.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict, Counter
-
-bob = defaultdict(Counter)
-for thing in 'qwertyuiop':
-    for otherthing in 'azertypoi':
-        bob[thing][otherthing] +=1
-
-print(bob)
-
